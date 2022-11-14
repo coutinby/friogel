104f95f0c9cb1dc4d66d3fafe4adb8df54791786
Nicolas Lima <nicolaslima@imac-de-nicolas.sistemafiergs.com.br>
2022-11-14 14:59:11 -0300

    adicionado server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const jsonServer = require('json-server');
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use('/api', jsonServer.router('db.json'));
+
+app.use(express.static(__dirname+'/dist/meu-projeto'));
+
+app.get('/*',(req,res) => {
+    res.sendFile(__dirname+'/dist/meu-projeto/index.html')
+});
+
+app.listen(PORT, () => {
+    console.log("Servidor iniciado na porta:" = PORT);
+})
